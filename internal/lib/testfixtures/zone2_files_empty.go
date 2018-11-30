8afed1293d5bdc3a1bcdf2f58e237065106257a2
Matthias Blaser <git@mooch.ch>
2018-11-30 23:13:56 +0100

    Fix vet errors

---
+++ internal/lib/testfixtures/zone2_files_empty.go
@@ -0,0 +1,10 @@
+// +build !fixtures
+
+package testfixtures
+
+import (
+	"github.com/maetthu/ngxcpd/internal/lib/proxycache"
+)
+
+// TestdataCacheFilesZone2 contains expected metadata of files in testdata/cache_files/zone1
+var TestdataCacheFilesZone2 = []*proxycache.Entry{}
--- internal/lib/testfixtures/zones.go
+++ internal/lib/testfixtures/zones.go
@@ -1,9 +1,8 @@
-// +build fixtures
-
 package testfixtures
 
 import "github.com/maetthu/ngxcpd/internal/lib/proxycache"
 
+// TestdataCacheFiles is a collection of test fixtures for different cache zones
 var TestdataCacheFiles = map[string][]*proxycache.Entry{
 	"zone1": TestdataCacheFilesZone1,
 	"zone2": TestdataCacheFilesZone2,
